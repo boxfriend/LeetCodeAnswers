753623131f4f96e4eeab0fcc2b55b245816efc22
Boxfriend <cawills394@gmail.com>
2022-02-07 00:00:19 -0500

    daily challenge 2/7

---
+++ Easy/389. Find the Difference.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public char FindTheDifference(string s, string t) {
+        var dict = new Dictionary<char, int>();
+        var charS = s.ToCharArray();
+        foreach(var ch in charS)
+        {
+            if(!dict.TryAdd(ch,0))
+                dict[ch]++;
+        }
+                
+        var dict2 = new Dictionary<char, int>();
+        var charT = t.ToCharArray();
+        foreach(var ch in charT)
+        {
+            if(!dict.ContainsKey(ch))
+                return ch;
+            
+            if(!dict2.TryAdd(ch,0))
+                dict2[ch]++;
+        }
+        
+        foreach(var pair in dict2)
+        {
+            if(dict[pair.Key] != pair.Value)
+                return pair.Key;
+        }
+        
+        return ' ';
+    }
+}
+//Runtime: 88 ms, faster than 88.36%
+//Memory Usage: 39.7 MB, less than 6.03%
