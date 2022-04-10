a514603e8089d5b74c2b254e7336d7b784dad427
Boxfriend <cawills394@gmail.com>
2022-04-10 09:46:01 -0400

    stack is slower but uses slightly less memory

--- Easy/682. Baseball Game.cs
+++ Easy/682. Baseball Game.cs
@@ -28,3 +28,35 @@ public class Solution {
 }
 //Runtime: 76 ms, faster than 96.19%
 //Memory Usage: 39.5 MB, less than 10.48%
+
+public class Solution1 {
+    public int CalPoints(string[] ops) {
+        var stack = new Stack<int>(); //Testing speed/mem using Stack<T> instead of List<T>
+        foreach(var s in ops)
+        {
+            if(s == "C")
+            {
+                stack.Pop();
+            } else if(s == "D")
+            {
+                stack.Push(stack.Peek()*2);
+            } else if (s == "+")
+            {
+                var a = stack.Pop();
+                var b = stack.Peek();
+                stack.Push(a);
+                stack.Push(a+b);
+            } else
+            {
+                stack.Push(Convert.ToInt32(s));
+            }
+        }
+        var total = 0;
+        while(stack.Count > 0)
+            total += stack.Pop();
+        
+        return total;
+    }
+}
+//Runtime: 115 ms, faster than 52.38%
+//Memory Usage: 38 MB, less than 76.19%
